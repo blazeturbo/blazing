ec26e88463acf626529547f80cdae7e66cc1c31b
astrid <astrid@localhost>
2026-09-17 21:12:55 +0200

    Let Stylix manage kitty background opacity

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -3,7 +3,6 @@
     enable = true;
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "0.90";
       window_padding_width = 10;
       scrollback_lines = 10000;
       enable_audio_bell = false;
