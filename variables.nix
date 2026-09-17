2dd58264b1cc0e8860ebe4423d7417d7a50593a8
astrid <astrid@localhost>
2026-09-17 21:59:55 +0200

    Add full ZaneyOS wallpaper collection and update variables.nix

--- variables.nix
+++ variables.nix
@@ -7,9 +7,18 @@
   locale = "en_US.UTF-8";
 
   # Stylix Wallpaper (Changing this changes the system-wide color palette!)
+  # Pick any wallpaper from ./wallpapers/ or add your own:
   stylixImage = ./wallpapers/Rainnight.jpg;
   # stylixImage = ./wallpapers/AnimeGirlNightSky.jpg;
+  # stylixImage = ./wallpapers/Anime-Purple-eyes.png;
   # stylixImage = ./wallpapers/mountainscapedark.jpg;
+  # stylixImage = ./wallpapers/beautifulmountainscape.jpg;
+  # stylixImage = ./wallpapers/zaney-wallpaper.jpg;
+  # stylixImage = ./wallpapers/nix-wallpaper-stripes-logo.png;
+  # stylixImage = ./wallpapers/purple_gasstation_abstract_dark_night.jpg;
+  # stylixImage = ./wallpapers/midnight-reflections-moonlit-sea.webp;
+  # stylixImage = ./wallpapers/lofi-Urban-Nightscape.webp;
+  # stylixImage = ./wallpapers/alena-aenami-cold.webp;
 
   # Default Terminal & App Launcher
   terminal = "kitty";
