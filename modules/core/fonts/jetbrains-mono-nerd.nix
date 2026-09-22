73128f7373f5f3fafc1fbe62a2b3f1ad8b9e137e
blazeturbo <nahildiabi8@gmail.com>
2026-09-22 13:04:22 +0200

    jetbrains mono nerd from user download

---
+++ modules/core/fonts/jetbrains-mono-nerd.nix
@@ -0,0 +1,22 @@
+# JetBrains Mono Nerd Font from the user's own download
+# (~/Downloads/JetBrainsMono.zip, Nerd Fonts release): the 4 core Mono
+# cuts live beside this file. Family name, verified by fc-scan:
+# "JetBrainsMono Nerd Font Mono".
+{ stdenvNoCC }:
+stdenvNoCC.mkDerivation {
+  pname = "astrid-jetbrains-mono-nerd";
+  version = "3.5.0";
+
+  src = ./.;
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm444 *.ttf -t $out/share/fonts/truetype
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "JetBrains Mono Nerd Font, user-provided build";
+    license = stdenvNoCC.lib.licenses.ofl;
+  };
+}
