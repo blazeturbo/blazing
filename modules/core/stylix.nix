71402985b0206cf0f5b50803fd2d9e798c0bf108
blazeturbo <nahildiabi8@gmail.com>
2026-09-22 12:50:19 +0200

    try iosevka mono

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -12,8 +12,8 @@ in {
     # (see environment.sessionVariables in hosts/nixos/default.nix).
     fonts = {
       monospace = {
-        package = pkgs.nerd-fonts.fira-code;
-        name = "FiraCode Nerd Font Mono";
+        package = pkgs.nerd-fonts.iosevka;
+        name = "Iosevka Nerd Font Mono";
       };
       sansSerif = {
         package = pkgs.montserrat;
