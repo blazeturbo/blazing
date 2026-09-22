54c44e1ae7e9179f7e4d723f0f8eab7fcd2debe5
blazeturbo <nahildiabi8@gmail.com>
2026-09-22 13:21:03 +0200

    back to dejavu nerd mono

--- modules/core/sddm.nix
+++ modules/core/sddm.nix
@@ -13,7 +13,7 @@ let
     background = vars.stylixImage;
     autoColor = false;
     accentColor = "#${c.base0D}";
-    fontFamily = "CaskaydiaCove Nerd Font Mono";
+    fontFamily = "DejaVuSansM Nerd Font Mono";
   };
 in lib.mkIf useSddm {
   # SDDM's greeter needs an X server behind it (SDDM's own Wayland mode +
