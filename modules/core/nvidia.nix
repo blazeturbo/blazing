d2453bcaa5f84609f5750eadc20aaac2461e4465
blazeturbo <nahildiabi8@gmail.com>
2026-09-22 20:10:00 +0200

    nvidia driver to stable, open stays on

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -12,6 +12,6 @@
     powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
