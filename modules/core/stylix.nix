27dbf04e8be0413e18657232a0f8eba8a3e5bbc4
blazeturbo <nahildiabi8@gmail.com>
2026-09-22 13:11:59 +0200

    try inconsolata mono

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -12,8 +12,8 @@ in {
     # (see environment.sessionVariables in hosts/nixos/default.nix).
     fonts = {
       monospace = {
-        package = pkgs.callPackage ./fonts/jetbrains-mono-nerd.nix { };
-        name = "JetBrainsMono Nerd Font Mono";
+        package = pkgs.nerd-fonts.inconsolata;
+        name = "Inconsolata Nerd Font Mono";
       };
       sansSerif = {
         package = pkgs.montserrat;
