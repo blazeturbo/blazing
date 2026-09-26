3a204d3f0cbacf411d296a19d5c9b87c55f3986f
blazeturbo <nahildiabi8@gmail.com>
2026-09-26 12:30:01 +0200

    nvm stable drivers didnt fix shit

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -10,8 +10,8 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 }
